8d8ca0ebbe7c8540c0b65502e5ba5ec16fa265a0
Nuskz <arielliasjr200@gmail.com>
2022-04-18 22:52:54 -0300

    Sort Simples

--- src/SortSimples.java
+++ src/SortSimples.java
@@ -2,15 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class SortSimples {
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-    // MANDAR NO URI
-
+    // Sort Simples
     public static void main(String[] args) throws IOException {
 
         // Scanner
@@ -24,30 +16,43 @@ public class SortSimples {
         // Fim do Scanner
         // UM
         if (A < B && A < C) {
+            System.out.println(A);
             if (B < C) {
-                System.out.println(A + " \n" + B + " \n" + C);
+                System.out.println(B);
+                System.out.println(C);
             } else if (C < B) {
-                System.out.println(A + " \n" + C + " \n" + B);
+                System.out.println(C);
+                System.out.println(B);
             }
+
         }
         // DOIS
         if (B < A && B < C) {
+            System.out.println(B);
             if (A < C) {
-                System.out.println(B + " \n" + A + " \n" + C);
+                System.out.println(A);
+                System.out.println(C);
             } else if (C < A) {
-                System.out.println(B + " \n" + C + " \n" + A);
+                System.out.println(C);
+                System.out.println(A);
             }
         }
         // TRÊS
         if (C < A && C < B) {
+            System.out.println(C);
             if (A < B) {
-                System.out.println(C + " \n" + A + " \n" + B);
+                System.out.println(A);
+                System.out.println(B);
             } else if (B < A) {
-                System.out.println(C + " \n" + B + " \n" + A);
+                System.out.println(B);
+                System.out.println(A);
             }
         }
         // Ordem escrita
-        System.out.println("\n" + A + "\n" + B + "\n" + C);
+        System.out.println("");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
     }
 
 }
